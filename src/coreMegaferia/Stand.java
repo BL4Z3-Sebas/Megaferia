5145dbd5ad14f93ab1b67ced7a19a2a2bd22534f
BL4Z3-Sebas <158209851+BL4Z3-Sebas@users.noreply.github.com>
2024-03-22 09:35:41 -0500

    cambio nuevo

--- src/coreMegaferia/Stand.java
+++ src/coreMegaferia/Stand.java
@@ -8,4 +8,23 @@ public class Stand {
     private int id;
     private float precio;
     private  ArrayList editoriales;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public void setEditoriales(ArrayList editoriales) {
+        this.editoriales = editoriales;
+    }
+
+    @Override
+    public String toString() {
+        return "Stand{" + "id=" + id + ", precio=" + precio + ", editoriales=" + editoriales + '}';
+    }
+    
+    
 }
